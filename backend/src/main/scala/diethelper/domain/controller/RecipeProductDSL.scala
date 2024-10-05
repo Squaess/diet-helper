3345b37c09ac8fe308a632262c491868b3a078cf
Bartosz Banasik <babartosz.bns@gmail.com>
2024-10-05 21:53:19 +0200

    Removing that shoudl be in common

--- backend/src/main/scala/diethelper/domain/controller/RecipeProductDSL.scala
+++
@@ -1,3 +0,0 @@
-package diethelper.domain.controller
-
-final case class RecipeProductDSL(name: String, quantity: Double)
--- backend/src/main/scala/diethelper/domain/db/ListCategory.scala
+++
@@ -1,6 +0,0 @@
-package diethelper.domain.db
-
-sealed trait ListCategory(value: String)
-case object Fridge extends ListCategory("fridge")
-case object Others extends ListCategory("others")
-case object Vegetables extends ListCategory("vegetables")
